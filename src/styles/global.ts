251af2186c7d2d876e1d7d75f41cce5a7afa0ce7
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-11-08 21:32:08 -0300

    :beers: chore: update initial structure and add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,34 @@
+import {createGlobalStyle} from 'styled-components';
+
+import githubBackground from '../assets/background.svg';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+    font-size: 62.5%;
+  } 
+
+  body {
+    color: #fff; 
+    background: #56595B url(${githubBackground}) no-repeat 70% top;
+    background-size: 500px;
+    
+    -webkit-font-smoothing: antialiased;
+  }
+  
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.6rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
