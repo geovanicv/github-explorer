4fb6db577f4103c3e652770cb1b0f9b26ca53774
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-11-09 20:32:47 -0300

    :sparkles: feat: create Explorer page

--- src/pages/Explorer/index.tsx
+++ src/pages/Explorer/index.tsx
@@ -1,9 +1,97 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import Header from '../../components/Header/Header';
+import RepositoryCard from '../../components/RepositoryCard';
+import UserCard from '../../components/UserCard';
 
+import {api} from '../../api';
 import * as S from './styles';
 
+interface User {
+  login: string;
+  avatar_url: string;
+  name: string;
+  bio: string;
+}
+
+interface Repository {
+  id: number;
+  full_name: string;
+  description: string;
+  stargazers_count: string;
+  forks_count: string;
+  language: string;
+  owner: {
+    avatar_url: string;
+  }
+}
+
 const Explorer = () => {
-  return <S.Container>Explorer</S.Container>;
+  const [filter, setFilter] = useState('repos');
+  const [newSearch, setNewSearch] = useState('');
+
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
+
+  async function handleSearch(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const response = await fetch(`${api}/${filter}/${newSearch}`);
+    const data = await response.json();
+
+      filter === 'repos' 
+      ? setRepositories([...repositories, data])
+      : setUsers([...users, data]);
+    
+    setNewSearch('');
+  }
+
+  return (
+    <S.Container>
+      <Header>
+        <S.Title>Explore no Github.</S.Title>
+        <S.Form onSubmit={handleSearch}>
+          <select value={filter} onChange={e => setFilter(e.target.value)}>
+            <option value="repos">Repositórios</option>
+            <option value="users">Usuários</option>
+          </select>
+          <input 
+            type="text" 
+            placeholder="Digite aqui" 
+            value={newSearch} 
+            onChange={e => setNewSearch(e.target.value)}
+          />
+          <button type="submit">Pesquisar</button>
+        </S.Form>
+      </Header>
+
+      <S.MainContent>
+        <h4>Recentes</h4>
+        {
+          filter === 'repos'
+          ? repositories.map(repo => (
+            <RepositoryCard
+              key={repo.id}
+              full_name={repo.full_name}
+              description={repo.description}
+              stargazers_count={repo.stargazers_count}
+              forks_count={repo.forks_count}
+              language={repo.language}
+              owner={repo.owner}
+            />
+          )).reverse()
+          : users.map(user => (
+            <UserCard 
+              key={user.login}
+              login={user.login}
+              avatar_url={user.avatar_url}
+              name={user.name}
+              bio={user.bio}
+            />
+          )).reverse()
+        }
+      </S.MainContent>
+    </S.Container>
+  );
 }
 
 export default Explorer;
--- src/pages/Explorer/styles.ts
+++ src/pages/Explorer/styles.ts
@@ -1,5 +1,66 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  max-width: 1114px;
+  margin: 0 auto;
+  padding: 0 2rem;
 `;
+
+export const Title = styled.h1`
+  margin-top: 9rem;
+`;
+
+export const Form = styled.form`
+  margin-top: 2.5rem;
+  margin-bottom: 9rem;
+  max-width: 100%;
+  display: flex;
+
+  select {
+    height: 5rem;
+    color: #A8A8B3;
+    border: 0;
+    padding: 0 12px;
+    background-color: #404344;
+
+    &:after {}
+  }
+
+  input {
+    flex: 1;
+    height: 5rem;
+    padding: 0 2.4rem;
+    border: 0;
+    background-color: #404344;
+    color: white;
+
+    &::placeholder {
+      color: #A8A8B3;
+    }
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+
+    width: 21rem;
+    height: 5rem;
+    border-radius: 0 5px 5px 0;
+
+    background: #16A862;
+    border: 0;
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+  }
+`;
+
+export const MainContent = styled.main`
+  > h4 {
+    padding: 0 0 1.4rem 0;
+  }
+`;
+
+
