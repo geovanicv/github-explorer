9b77d2302f9407a9c6c96edf9d8aa87eb22ce039
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-11-09 20:31:38 -0300

    :bento: chore: ads new icons

--- src/pages/Trending/index.tsx
+++ src/pages/Trending/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Header from '../../components/Header/Header';
 import searchIcon from '../../assets/search.svg';
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -23,7 +23,7 @@ export default createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
   
-  body, input, button {
+  body, input, button, select {
     font-family: 'Roboto', sans-serif;
     font-size: 1.6rem;
   }
