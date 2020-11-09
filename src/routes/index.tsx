c0fabbbd7db3adb077d6d5a5c19cf860a298e018
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-11-08 21:32:49 -0300

    :sparkles: feat: create routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+import Trending from '../pages/Trending';
+import Explorer from '../pages/Explorer';
+import Repository from '../pages/Repository';
+import Profile from '../pages/Profile';
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact component={Trending} />
+    <Route path="/explorer" component={Explorer} />
+    <Route path="/repository" component={Repository} />
+    <Route path="/profile" component={Profile} />
+  </Switch>
+);
+
+export default Routes;
+
