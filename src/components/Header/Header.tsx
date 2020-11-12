131b1b97d873870c87a789b4db7150766b3ca464
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-11-11 23:42:04 -0300

    :art: chore: add Link from react-router-dom to header goback button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
 import arrowLeftIcon from '../../assets/arrow-left.svg';
 
 import * as S from './styles';
 
 type Props = {
-  haveButtonBack?: boolean;
+  haveButtonBack?: string;
   children?: React.ReactNode;
 }
 
 const Header = ({children, haveButtonBack}: Props) => {
+  
   return (
     <S.Container>
       <S.NavContent>
         <img src={logoImg} alt="github explorer"/>
         {haveButtonBack ? (
-           <S.BackButton>
-           <img src={arrowLeftIcon} alt="Voltar"/>Voltar
-         </S.BackButton>
+          <Link to={haveButtonBack}>
+            <img src={arrowLeftIcon} alt="Voltar"/>Voltar
+          </Link>
         ) : (
           <div />
         )}
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,29 +2,32 @@ import styled from 'styled-components';
 
 export const Container = styled.header`
   width: 100%;  
+  padding: 20px 0;
 `;
 
 export const NavContent = styled.nav`
-  padding: 20px 0;
-
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
 
-export const BackButton = styled.button`
-  background: transparent;
-  border: 0;
-  color: #A8A8B3;
-  font-weight: bold;
+  a { 
+    background: transparent;
+    border: 0;
+    color: #A8A8B3;
+    font-weight: bold;
+    text-decoration: none;
 
-  display: flex;
-  align-items: center;
+    display: flex;
+    align-items: center;
 
-  img {
-    margin-right: 12px;
+    img {
+      margin-right: 12px;
+    }
   }
 `;
+
+
+  
 
 
 
