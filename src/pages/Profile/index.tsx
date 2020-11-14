c8e092753484069cb2dda8630df7b969108fa081
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-11-13 23:44:10 -0300

    :sparkles: feat: create Profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/Header/Header';
+import RepoBasic from '../../components/RepoBasic/RepoBasic';
+import ProfileUserCard from '../../components/ProfileUserCard';
+import { useRouteMatch } from 'react-router-dom';
+
+import {api} from '../../api';
 
 import * as S from './styles';
 
+interface User {
+  login: string;
+  avatar_url: string;
+  name: string;
+  bio: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+}
+
+interface Repos {
+  name: string;
+  description: string;
+  language: string;
+  full_name: string;
+}
+
+interface ProfileParams {
+  login: string;
+}
+
 const Profile = () => {
-  return <S.Container>Profile</S.Container>;
+  const [user, setUser] = useState<User | null>(null);
+  const [repos, setRepos] = useState<Repos[]>([])
+
+  const {params} = useRouteMatch<ProfileParams>();
+
+  useEffect(() => {
+    async function loadProfileData(): Promise<void> {
+      fetch(`${api}/users/${params.login}`)
+        .then(response => response.json())
+        .then(response => setUser(response));
+
+      fetch(`${api}/users/${params.login}/repos`)
+        .then(response => response.json())
+        .then(response => setRepos(response));
+    }
+
+    loadProfileData();
+
+  }, [params.login])
+
+  return (
+    <S.Container>
+      <Header haveButtonBack="/explorer"/>
+      <S.Content>
+        { user && 
+            <ProfileUserCard 
+              avatar_url={user.avatar_url}
+              bio={user.bio}
+              followers={user.followers}
+              following={user.following}
+              login={user.login}
+              name={user.name}
+              public_repos={user.public_repos}
+            />
+        }
+        <S.RepositoriesList>
+          <strong>Repositórios</strong>
+          { repos && repos.map(item => (
+              <RepoBasic 
+                key={item.name}
+                description={item.description}
+                language={item.language}
+                name={item.name}
+                full_name={item.full_name}
+              />
+          ))
+
+          }
+        </S.RepositoriesList>
+      </S.Content>
+
+    </S.Container>
+  );
 }
 
 export default Profile;
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  max-width: 1114px;
+  margin: 0 auto;
+  padding: 2rem;
+
 `;
+
+export const Content = styled.div`
+  margin-top: 4rem;
+  display: flex;
+`;
+
+export const RepositoriesList = styled.div`
+  flex: 1;
+  margin-right: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+
