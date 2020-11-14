4cc671791ea464b3b5d5bde7334fd8a5b5bae86b
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2020-11-13 23:46:11 -0300

    :truck: chore: update route path

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import Trending from '../pages/Trending';
 import Explorer from '../pages/Explorer';
 import Repository from '../pages/Repository';
 import Profile from '../pages/Profile';
 
 const Routes = () => (
   <Switch>
-    <Route path="/" exact component={Trending} />
     <Route path="/explorer" component={Explorer} />
     <Route path="/repository/:reponame+" component={Repository} />
-    <Route path="/profile" component={Profile} />
+    <Route path="/profile/:login" component={Profile} />
   </Switch>
 );
 
